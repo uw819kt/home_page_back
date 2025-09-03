d1c4def5b5a74977930b93632e5ddd98a319b3d3
uw819kt <miya0arima@gmail.com>
2025-09-03 16:38:44 +0900

    feat: modelにvalidation追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+  validates :title, :tech_stack, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :status, :demo_url, presence: true
+  validates :github_url, format: { 
+    with: /\Ahttps:\/\/github\.com\/[\w-]+\/[\w-]+\z/, 
+    message: "must be a valid GitHub repository URL" 
+  }
 end
